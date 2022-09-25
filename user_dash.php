d47cf5cd03def46466988ba49351dfc2e02037c6
Muhammad-Arsal <charsal13579@gmail.com>
2022-09-25 18:08:18 +0500

    Ecomm

--- user_dash.php
+++ user_dash.php
@@ -1,4 +1,9 @@
-<?php ?>
+<?php 
+require_once "core/database.php";
+session_start();
+$user_id = $_SESSION['user_login'];
+$ordered_details = select_where("ordered_products","user_id",$user_id,$connection,2);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -67,87 +72,51 @@
 </head>
 
 <body>
-    <div class="container mt-5">
-        <div class="d-flex justify-content-center row">
-            <div class="col-md-10">
-                <div class="rounded">
-                    <div class="table-responsive table-borderless">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th class="text-center">
-                                        <div class="toggle-btn">
-                                            <div class="inner-circle"></div>
-                                        </div>
-                                    </th>
-                                    <th>Order #</th>
-                                    <th>Company name</th>
-                                    <th>status</th>
-                                    <th>Total</th>
-                                    <th>Created</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody class="table-body">
-                                <tr class="cell-1">
-                                    <td class="text-center">
-                                        <div class="toggle-btn">
-                                            <div class="inner-circle"></div>
-                                        </div>
-                                    </td>
-                                    <td>#SO-13487</td>
-                                    <td>Gasper Antunes</td>
-                                    <td><span class="badge badge-success">Fullfilled</span></td>
-                                    <td>$2674.00</td>
-                                    <td>Today</td>
-                                    <td><i class="fa fa-ellipsis-h text-black-50"></i></td>
-                                </tr>
-                                <tr class="cell-1">
-                                    <td class="text-center">
-                                        <div class="toggle-btn">
-                                            <div class="inner-circle"></div>
-                                        </div>
-                                    </td>
-                                    <td>#SO-13453</td>
-                                    <td>Aartsen van</td>
-                                    <td><span class="badge badge-info">Confirmed</span></td>
-                                    <td>$3454.00</td>
-                                    <td>Yesterday</td>
-                                    <td><i class="fa fa-ellipsis-h text-black-50"></i></td>
-                                </tr>
-                                <tr class="cell-1">
-                                    <td class="text-center">
-                                        <div class="toggle-btn">
-                                            <div class="inner-circle"></div>
-                                        </div>
-                                    </td>
-                                    <td>#SO-13498</td>
-                                    <td>Trashes Habard</td>
-                                    <td><span class="badge badge-danger">Partially shipped</span></td>
-                                    <td>$6274.00</td>
-                                    <td>May 12,2020</td>
-                                    <td><i class="fa fa-ellipsis-h text-black-50"></i></td>
-                                </tr>
-                                <tr class="cell-1">
-                                    <td class="text-center">
-                                        <div class="toggle-btn">
-                                            <div class="inner-circle"></div>
-                                        </div>
-                                    </td>
-                                    <td>#SO-16499</td>
-                                    <td>Samban Hubart</td>
-                                    <td><span class="badge badge-success">Fullfilled</span></td>
-                                    <td>$6375.00</td>
-                                    <td>May 11,2020</td>
-                                    <td><i class="fa fa-ellipsis-h text-black-50"></i></td>
-                                </tr>
-                            </tbody>
-                        </table>
+    <div class="container">
+        <h2 class="text-center mt-4"><u>User Dashboard</u></h2>
+        <div class="mt-5">
+            <div class="d-flex justify-content-center row">
+                <div class="col-md-10">
+                    <div class="rounded">
+                        <div class="table-responsive table-borderless">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Order #</th>
+                                        <th>Company name</th>
+                                        <th>status</th>
+                                        <th>Total</th>
+                                        <th>Created</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="table-body">
+                                    <?php foreach($ordered_details as $all_details){
+                                         $products_id = $all_details['product_id'];
+                                         $seller_id = select_where("products","id",$products_id,$connection,1);
+                                         $seller_updated_id = $seller_id['seller'];
+                                         $seller_name = select_where("seller","id",$seller_updated_id,$connection,1);
+                                         ?>
+                                    <tr class="cell-1">
+                                        <td>#U-<?php echo $all_details['order_number'] ?></td>
+                                        <td><?php echo $seller_name['seller_name']; ?></td>
+                                        <td><span
+                                                class="badge badge-success"><?php if($all_details['status'] == 0){
+                                            echo "Dispatched";
+                                        }elseif($all_details['status'] == 1){ echo "On the way";}elseif($all_details['status']==2){ echo "Delivered";} ?></span>
+                                        </td>
+                                        <td>$<?php echo $all_details['total_price'] ?></td>
+                                        <td><?php echo $all_details['created_on'] ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
     $(document).ready(function() {
